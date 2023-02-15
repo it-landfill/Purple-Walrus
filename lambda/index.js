f61295701a4c93fca32f75f88a91e25c474add4d
Alessandro Benetton <aleben98@gmail.com>
2023-02-15 17:28:38 +0100

    [master] Disabled source map

--- lambda/index.js
+++ lambda/index.js
@@ -1,47 +1,40 @@
+"use strict";
 const Alexa = require("ask-sdk-core");
-
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return (Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest");
     },
     handle(handlerInput) {
         const speakOutput = "Test skill launch";
-
         return handlerInput.responseBuilder.speak(speakOutput).reprompt(speakOutput).getResponse();
     }
 };
-
 const HelloWorldIntentHandler = {
     canHandle(handlerInput) {
         return (Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" && Alexa.getIntentName(handlerInput.requestEnvelope) === "NextClassIntent");
     },
     handle(handlerInput) {
         const speakOutput = "Hello World!";
-
         return (handlerInput.responseBuilder.speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse());
     }
 };
-
 const HelpIntentHandler = {
     canHandle(handlerInput) {
         return (Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" && Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.HelpIntent");
     },
     handle(handlerInput) {
         const speakOutput = "You can say hello to me! How can I help?";
-
         return handlerInput.responseBuilder.speak(speakOutput).reprompt(speakOutput).getResponse();
     }
 };
-
 const CancelAndStopIntentHandler = {
     canHandle(handlerInput) {
         return (Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" && (Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.CancelIntent" || Alexa.getIntentName(handlerInput.requestEnvelope) === "AMAZON.StopIntent"));
     },
     handle(handlerInput) {
         const speakOutput = "Goodbye!";
-
         return handlerInput.responseBuilder.speak(speakOutput).getResponse();
     }
 };
@@ -52,7 +45,6 @@ const FallbackIntentHandler = {
     },
     handle(handlerInput) {
         const speakOutput = "Sorry, I don't know about that. Please try again.";
-
         return handlerInput.responseBuilder.speak(speakOutput).reprompt(speakOutput).getResponse();
     }
 };
@@ -75,7 +67,6 @@ const IntentReflectorHandler = {
     handle(handlerInput) {
         const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
         const speakOutput = `You just triggered ${intentName}`;
-
         return (handlerInput.responseBuilder.speak(speakOutput)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse());
@@ -93,11 +84,9 @@ const ErrorHandler = {
     handle(handlerInput, error) {
         const speakOutput = "Sorry, I had trouble doing what you asked. Please try again.";
         console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
-
         return handlerInput.responseBuilder.speak(speakOutput).reprompt(speakOutput).getResponse();
     }
 };
-
 /**
  * This handler acts as the entry point for your skill, routing all request and response
  * payloads to the handlers above. Make sure any new handlers or interceptors you've
