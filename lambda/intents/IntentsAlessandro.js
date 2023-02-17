3f0614882502916f3310b9ff0ca01ef4bacb0c52
Alessandro Benetton <aleben98@gmail.com>
2023-02-17 17:28:29 +0100

    [master] Cjeje

--- lambda/intents/IntentsAlessandro.js
+++ lambda/intents/IntentsAlessandro.js
@@ -13,12 +13,14 @@ var IntentsAlessandro;
             const speakOutput = "Hello World!";
             const region = process.env.DYNAMODB_PERSISTENCE_REGION || "eu-west-1";
             const client = new AWS.DynamoDB({ region });
+            console.warn("AJEJEJ");
             client.listTables({}, (err, data) => {
                 if (err)
                     console.warn(err, err.stack);
                 else
                     console.warn(data);
             });
+            console.warn("BJEJEJ");
             return (handlerInput.responseBuilder.speak(speakOutput)
                 //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
                 .getResponse());
--- src/intents/IntentsAlessandro.ts
+++ src/intents/IntentsAlessandro.ts
@@ -12,6 +12,7 @@ export module IntentsAlessandro {
 
 			const region = process.env.DYNAMODB_PERSISTENCE_REGION || "eu-west-1";
 			const client = new AWS.DynamoDB({region});
+			console.warn("AJEJEJ");
 			client.listTables({}, (err : any, data : any) => {
 				if (err) 
 					console.warn(err, err.stack);
@@ -19,6 +20,7 @@ export module IntentsAlessandro {
 					console.warn(data);
 				}
 			);
+			console.warn("BJEJEJ");
 
 			return (handlerInput.responseBuilder.speak(speakOutput)
 			//.reprompt('add a reprompt if you want to keep the session open for the user to respond')
