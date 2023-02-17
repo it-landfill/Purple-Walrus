d8548fc9924b8795fe1d08d0eff550f8fa5171b3
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2023-02-17 16:13:55 +0000

    deleted 14 files.

--- lambda/intents/intentsLorenzo.d.ts
+++
@@ -1,7 +0,0 @@
-import Alexa = require("ask-sdk-core");
-export declare module IntentsLorenzo {
-    const HelloWorldIntentHandler: {
-        canHandle(handlerInput: Alexa.HandlerInput): boolean;
-        handle(handlerInput: Alexa.HandlerInput): import("ask-sdk-model").Response;
-    };
-}
